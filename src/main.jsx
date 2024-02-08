4a65cb710cc16e70b17c7a5bffa44607cbbe52fa
AbolfazlKarbakhsh <Abolfazkarbakhsh@gmail.com>
2024-02-08 22:26:28 +0330

    start project
    -- create vite package
    -- install dependensis

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './assets/sass/main.scss'
+
+ReactDOM.createRoot(document.getElementById('Tizhoo_Ui')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
